47576e118eb9dd54853b0444426afbbbff11b817
jjmschofield <jjmschofield@googlemail.com>
2018-04-08 13:55:58 +0100

    Adding method to retrieve bar by ID only

--- src/api/barsApi.js
+++ src/api/barsApi.js
@@ -2,9 +2,12 @@ import { barData } from './barsApi.data';
 
 export function getBarsNearLocation({ lat, long }) {
   console.log('Getting bars near', { lat, long });
-  console.log(barData);
-
   return Promise.resolve({ data: barData });
 }
 
-export default getBarsNearLocation;
+export function getBarWithId(id) {
+  const filteredBars = barData.filter((bar) => {
+    return bar.id === id;
+  });
+  return Promise.resolve({ data: filteredBars[0] });
+}
